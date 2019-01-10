8ccd3af85ee0520dc925f16a1e79e312680167da
sonishilpa1906 <sonishilpa1906@gmail.com>
2019-01-10 16:55:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,27 @@ const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
+	
+	
+	function is_touch_device() {
+  var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+  var mq = function(query) {
+    return window.matchMedia(query).matches;
+  }
+
+  if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+    return true;
+  }
+
+  // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+  // https://git.io/vznFH
+  var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+  return mq(query);
+}
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
+	alert(is_touch_device());
 	try
 	{
 		//setTimeout(function(){ cameraView.removeAttribute("controls"); }, 1000);
